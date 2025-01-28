3c2ca3597a325dd9c0a38940697ff3d98051f1bb
Daniel <ochigbodaniel240@gmail.com>
2025-01-28 13:33:14 +0100

    first commit

---
+++ scr/app.test.js
@@ -0,0 +1,12 @@
+const app = require('./app')
+const supertest = require('supertest')
+const request = supertest(app)
+
+describe("/ endpoint", ()=>{
+    it("should return a response", async() =>{
+        const response = await request.get("/")
+        expect(response.status).toBe(200)
+        expect(response.text).toBe("the end point works");
+        
+    })
+})
