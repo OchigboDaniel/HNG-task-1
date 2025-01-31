5c651de5b0812c39be32a377ad9b7bdf1f7497cf
Daniel <ochigbodaniel240@gmail.com>
2025-01-31 10:03:00 +0100

    Updated github actios to get books

--- scr/app.test.js
+++ scr/app.test.js
@@ -6,6 +6,6 @@ describe("Book api", () => {
   it("It should retive book records", async () => {
     const response = await request.get("/api/v1/books");
     expect(response.status).toBe(200);
-    expect(response.text).toBe("Book details recieve successfully.");
+    expect(response.body.message).toBe("Book details recieve successfully.");
   });
 });
